abf183fc5dae44dad85cef02d7202ebb883edb63
Donveds <donveds@gmail.com>
2021-06-04 13:56:17 +0300

    Выделил нужные строчки для последующей обработки

--- js/create.js
+++ js/create.js
@@ -1,1 +1,15 @@
+function create(){
+  
+  let ZoomLink = document.getElementById("ZoomLink").value.split(/\r?\n/);
 
+  //Выделил нужные строчки для последующей обработки
+  let TimeAndDateLine = ZoomLink[0];
+  let LinkLine = ZoomLink[2];
+  let MeetingIDLine = ZoomLink[3];
+  let PasscodeLine = ZoomLink[4];
+
+}
+
+function cancel(){
+  document.getElementById("ZoomLink").value = "";
+}
